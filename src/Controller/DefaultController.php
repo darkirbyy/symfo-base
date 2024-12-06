ec530c32cdc01d7ec5819af821198bfe9c56c562
darkirby <darkirby.tuta.com>
2024-12-06 14:27:18 +0100

    Update default page to check modules

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,13 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\DBAL\Connection;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_default')]
-    public function index(): Response
+    public function index(Connection $connection): Response
     {
+        try{
+            $connection->getDatabase();
+            $db_test = true;
+        }
+        catch (\Exception $e){
+            $db_test = false;
+        }
         return $this->render('default/index.html.twig', [
+            'db_test' => $db_test,
         ]);
     }
 }
