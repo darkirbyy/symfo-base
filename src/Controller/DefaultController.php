97275ac9cb74dfa386a59586a04a27c25a4370ee
darkirby <darkirby.tuta.com>
2024-12-06 22:07:06 +0100

    Make githooks executable

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,23 +2,23 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Doctrine\DBAL\Connection;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_default')]
     public function index(Connection $connection): Response
     {
-        try{
+        try {
             $connection->getDatabase();
             $db_test = true;
-        }
-        catch (\Exception $e){
+        } catch (\Exception $e) {
             $db_test = false;
         }
+
         return $this->render('default/index.html.twig', [
             'db_test' => $db_test,
         ]);
