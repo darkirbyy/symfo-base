fa9179db1c5da3ea11fd9239a1e90408d3811621
darkirby <darkirby.tuta.com>
2025-01-28 00:25:02 +0100

    Improve minor config parameters

--- linter/.twig-cs-fixer.dist.php
+++ linter/.twig-cs-fixer.dist.php
@@ -7,6 +7,8 @@ $finder = (new TwigCsFixer\File\Finder())
 $ruleset = new TwigCsFixer\Ruleset\Ruleset();
 $ruleset->addStandard(new TwigCsFixer\Standard\TwigCsFixer());
 
+// $ruleset->addRule(new TwigCsFixer\Rules\File\FileExtensionRule());
+// $ruleset->removeRule(TwigCsFixer\Rules\Whitespace\EmptyLinesRule::class);
 $ruleset->overrideRule(new TwigCsFixer\Rules\Punctuation\PunctuationSpacingRule(
     ['}' => 1],
     ['{' => 1],
